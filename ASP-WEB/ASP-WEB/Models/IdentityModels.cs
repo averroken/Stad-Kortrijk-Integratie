c4b2b358bf6c0d06517a450184fb705c9e0c764e
LGMeersseman <louis-guy.meersseman@student.howest.be>
2016-05-25 15:38:32 +0200

    Changed Identity to correct db

--- ASP-WEB/ASP-WEB/Models/IdentityModels.cs
+++ ASP-WEB/ASP-WEB/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ASP_WEB.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("IntegratieContext", throwIfV1Schema: false)
         {
         }
 
