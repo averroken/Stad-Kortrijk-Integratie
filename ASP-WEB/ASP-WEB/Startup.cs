7abe817f7dc7d69e34a83636c003358b7c91810d
LGMeersseman <louis-guy.meersseman@student.howest.be>
2016-05-25 13:10:04 +0200

    Added ASP.NET Project

---
+++ ASP-WEB/ASP-WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_WEB.Startup))]
+namespace ASP_WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
