8f14ee90b0f656c48b7ce85b18c4fa15ac74839a
LGMeersseman <louis-guy.meersseman@student.howest.be>
2016-05-26 15:05:45 +0200

    Viewmodel for searches

---
+++ ASP-WEB/ASP-WEB/Models/FaqSubthemeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_WEB.Models
+{
+    public class FaqSubtheme
+    {
+        public List<Faq> Faq { get; set; }
+        public List<Subtheme> Subtheme { get; set; }
+    }
+}
