8c4a20179d93273d02b13357f0b329bebfda37b4
DarthSwedo <matthieu19@msn.com>
2016-06-01 13:00:03 +0200

    12:59

--- ASP-WEB/ASP-WEB/ASP-WEB/App_Start/BundleConfig.cs
+++ ASP-WEB/ASP-WEB/ASP-WEB/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace ASP_WEB
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/Script.js"));
         }
     }
 }
