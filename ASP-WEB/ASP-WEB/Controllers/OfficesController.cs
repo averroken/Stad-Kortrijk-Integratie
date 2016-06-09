dc59e1adf03fff8ee2eda73330adfc8e9f9e2f90
LGMeersseman <louis-guy.meersseman@student.howest.be>
2016-06-09 11:56:32 +0200

    Added API for offices by subtheme

---
+++ ASP-WEB/ASP-WEB/Controllers/OfficesController.cs
@@ -0,0 +1,27 @@
+﻿using ASP_WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP_WEB.Controllers
+{
+    public class OfficesController : ApiController
+    {
+        GenericRepository<Subtheme> repoSubtheme = new GenericRepository<Subtheme>();
+        GenericRepository<Office> repoOffice = new GenericRepository<Office>();
+
+        public IHttpActionResult GetOfficesBySubthemes(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return Ok(repoOffice.All());
+            }
+            var offices = repoSubtheme.GetByID(id).Office;
+
+            return Ok(offices);
+        }
+    }
+}
